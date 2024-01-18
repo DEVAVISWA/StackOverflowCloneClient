07ac46ffa1124c872faa6d3a10e304cef868314a
DEVAVISWA G <devaviswa15@gmail.com>
2024-01-18 17:27:31 +0530

    change in body content

--- src/Components/BodyDummyContent.jsx
+++ src/Components/BodyDummyContent.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 function BodyDummyContent() {
     return (
-        <div>
+        <div className='container-fluid'>
             <h1> Every
                 <div className="js-user-slider p-home-tagline va-bottom ps-relative ws-nowrap d-inline-flex fd-column ai-center fc-orange-400 lg:d-block mx-auto ta-center js-loaded" data-words="developer, data scientist, system admin, mobile developer, game developer" >
                     <div id="containerAnimation" className="container-bottom-to-up" ><p className="top-to-bottom">Developer</p></div> &nbsp; &nbsp;
                     <div id="containerAnimation" className="container-bottom-to-up" >   <p className="bottom-to-up">GameDeveloper</p>  </div> &nbsp; &nbsp;
                     <div id="containerAnimation" className="container-bottom-to-up" ><p className="top-to-bottom">MobileDeveloper</p>  </div> &nbsp; &nbsp;
                     <div id="containerAnimation" className="container-bottom-to-up" >   <p className="bottom-to-up">DataScientist</p>  </div> &nbsp; &nbsp;
-                </div> has a<br className="lg:d-none" /> tab open to<br className="d-none lg:d-block" /> Stack Overflow
+                </div> has a<br className="lg:d-none" /> tab open to<br className="d-none lg:d-block" /> Stack Overflow 
             </h1>
         </div>
     )
